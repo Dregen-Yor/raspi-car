76d1253e5211d892db0fd8fdc4f4654e28d7bc84
Dregen_Yor <hgcatqdao@outlook.com>
2024-05-04 19:47:48 +0800

    give up rust

--- raspi-car-control/src/App.vue
+++ raspi-car-control/src/App.vue
@@ -1,44 +1,72 @@
 <template>
   <main>
     <div class="flex flex-col items-center justify-center">
-      <h1>Raspi Car Control</h1>
-      <iframe src="http://127.0.0.1:8081/" width="400px" height="350px"></iframe>
+      
+      <h1 class="text-2xl">Raspi Car Control</h1>
+      <br>
+      <iframe src="http://192.168.35.158:8081/0/stream" width="1280px" height="720px"></iframe>
     </div>
-    
-    
+    <div class="flex items-center justify-center space-x-10">
+      <div class="">
+        <p>温度：{{ T }}</p>
+        <p>湿度：{{ H }}</p>
+        <p>气压：{{ P }}</p>
+        <p>距离：{{ D }}</p>
+      </div>
+      <div class="flex">
+        <el-text class=" w-14">速度: </el-text><el-input  v-model="speed" placeholder="速度范围为 0-100" maxlength="2"></el-input>
+      </div>
+    </div>
   </main>
 </template>
 <script>
 import axios from 'axios';
 export default {
   data() {
-    return {
+    return {  
       T:0,
       P:0,
       H:0,
       D:0,
+      speed:100,
     }
   },
   mounted() {
+    window.addEventListener('keydown', this.handleKeyDown);
     clearInterval(this.intervalTime)
     this.intervalTime = setInterval(() => {
+      this.sync_data();
+      console.log("succ");
     }, 5000);
+  },
+  beforeUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
   },
   methods: {
     sync_data(){
-      axios.get("http://127.0.0.1:5800/getT").then(response =>{
+      axios.get("http://124.70.51.81:808/getT").then(response =>{
         this.T=response.data;
       });
-      axios.get("http://127.0.0.1:5800/getP").then(response =>{
+      axios.get("http://124.70.51.81:808/getP").then(response =>{
         this.P=response.data;
       });
-      axios.get("http://127.0.0.1:5800/getH").then(response =>{
+      axios.get("http://124.70.51.81:808/getH").then(response =>{
         this.H=response.data;
       });
-      axios.get("http://127.0.0.1:5800/getD").then(response =>{
+      axios.get("http://124.70.51.81:808/getD").then(response =>{
         this.D=response.data;
       });
-    }
+    },
+    handleKeyDown(event) {
+      if (event.key === 'W') {
+        axios.post()
+      }
+    },
   }
 }
 </script>
+<style scoped>
+.el-input{
+  width: 60px;
+}
+</style>
--- raspi-car-control/src/main.js
+++ raspi-car-control/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './index.css'
 import {Axios} from 'axios'
 import VueAxios from 'vue-axios'
+import Element from 'element-plus'
+import 'element-plus/dist/index.css'
 const app = createApp(App)
-app.use(VueAxios,Axios)+
+app.use(Element)
+app.use(VueAxios,Axios)
 app.mount('#app')
